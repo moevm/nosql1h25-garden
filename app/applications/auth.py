760730d69b0912ee6987625b19bd85dca868aad2
dvikovdaniil <danillafuture@gmail.com>
2025-05-17 13:31:06 +0300

    little change structure

--- app/applications/auth.py
+++ app/applications/auth.py
@@ -2,8 +2,7 @@ from flask import (
     Blueprint, render_template
 )
 
-auth_bp = Blueprint("auth_bp", __name__, template_folder=".templates")
-
+auth_bp = Blueprint("auth_bp", __name__, template_folder="../templates")
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
