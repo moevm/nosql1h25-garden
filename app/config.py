47e34cb9c4bf76ce7c8c66b9fe6adea7b1aa1816
Daryapotapovaa <dm.potapova@gmail.com>
2025-05-22 17:16:27 +0300

    Upgrade config

--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
 import os
+
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://garden_user:garden_password@db:27017/garden_db?authSource=admin')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
+    
+    # Flask-PyMongo settings
+    MONGO_AUTH_SOURCE = 'admin'
+    MONGO_AUTH_MECHANISM = 'SCRAM-SHA-256'
